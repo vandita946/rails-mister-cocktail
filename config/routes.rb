2e751a5f4eda0e287bda1bcb730daa3bceff2e18
Vandita Attal <vandita.attal@gmail.com>
2020-06-25 12:50:10 +0800

    have finished up to doses form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # cocktail routes
+  # index - user can see list of cocktails
+  # show - user can see details of a given cocktail
+  # create & new - user can create a new cocktail
+
+  # dose routes
+  # new - user can add a new dose to an existing cocktail (ingredient/description pair)
+  # delete - user can delete dose that belongs to an existing cocktail
+
+  resources :cocktails, only: [:index, :show, :create, :new] do
+    resources :doses, only: [:new, :create, :destroy]
+  end
+
 end
